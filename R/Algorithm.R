cad396803ab2d1b8bfe328d947624b89a3f224ea
JENScoding <jens.lichter@posteo.de>
2019-08-18 23:43:04 +0200

    plots in functions

--- R/Algorithm.R
+++ R/Algorithm.R
@@ -10,9 +10,6 @@ weights_x = c(1e+3, 1e+3, 5e+2, 8e+2, 2e+2, 8e+1, 6e+1, 2e+1, 1.8, 1)
 
 ## function for algo
 
-lm_OLS <- lm(y~x)
-slope <- as.numeric(lm_OLS[[1]][2])
-
 york <- function(y, x, tolerance = 1e-10, weights.y, weights.x){
   #initial value of b is OLS
   lm_OLS <- lm(y~x)
@@ -75,33 +72,74 @@ york <- function(y, x, tolerance = 1e-10, weights.y, weights.x){
 
   c <- 0*alpha
 
-  x_resdiduals <- (Weight * (intercept + slope * x - y) * (c - slope * weights_y)) / (weights_y * weights_x)
+  x_residuals <- (Weight * (intercept + slope * x - y) * (c - slope * weights_y)) / (weights_y * weights_x)
 
-  y_resdiduals <- (Weight * (intercept + slope * x - y) * (weights_x - slope * c))/ (weights_y * weights_x)
+  y_residuals <- (Weight * (intercept + slope * x - y) * (weights_x - slope * c))/ (weights_y * weights_x)
 
+  create.plots <- function(x, y){
+    plot(x, y, pch = 16)
+    lines(x, fitted_y , col = "red",lwd = 2)
+    lines(x, lm_OLS$fitted.values, col = "blue", lty = "dashed",lwd = 2)
+    legend("topright",legend = c("OLS","York"), fill = c("blue","red"))
+    #the York regression line and the OLS regression line both go to the "center of gravity"
+    abline(v = x_bar, h = y_bar ,lty = "dashed", col = "red")
+    points(x = x_bar, y = y_bar, col = "red", pch = 16)
+    abline(v = mean(x), h = mean(y) ,lty = "dashed", col = "blue")
+    points(x = mean(x), y = mean(y), col = "blue", pch = 16)
+    compare_OLS_York <- recordPlot()
+    dev.off()
+
+    plot(x_residuals, y_residuals)
+    residuals <- recordPlot()
+    dev.off()
+
+    return(list("compare_OLS_York" = compare_OLS_York, "residuals" = residuals))
+  }
+  all.plots <- create.plots(x, y)
 
 
   mt <- matrix(c(intercept, slope, sigma_intercept, sigma_slope), nrow = 2)
   rownames(mt) <- c("intercept", "slope")
   colnames(mt) <- c("Estimate", "Std.Error")
-  est <- list("coefficients" = mt, "weighting.vector" = Weight, "x.residuals" = x_resdiduals,"y.residuals"=y_resdiduals,"fitted_y"=fitted_y, "mean.x" = x_bar, "mean.y" = y_bar ,"Std.Error.chi" = sigma_x, "iterations" = count, x_centered, y_centered, x, y, x_mean, "show" = x_adj)
+  est <- list("coefficients" = mt,
+              "weighting.vector" = Weight,
+              "x.residuals" = x_residuals,
+              "y.residuals"=y_residuals,
+              "fitted_y"=fitted_y,
+              "mean.x" = x_bar,
+              "mean.y" = y_bar ,
+              "Std.Error.chi" = sigma_x,
+              "iterations" = count,
+              x_centered, y_centered, x, y, x_mean, "show" = x_adj,
+              "plots" = list("compare_OLS_York" = all.plots$compare_OLS_York, "residuals" = all.plots$residuals))
   return(est)
 }
 
 first <- york(y, x, weights.y = weights_y, weights.x = weights_x)
-first
 
-plot(x,y, pch = 16)
-lines(x,first$fitted_y , col = "red",lwd = 2)
-lines(x, lm_OLS$fitted.values, col = "blue", lty = "dashed",lwd = 2)
-legend("topright",legend = c("OLS","York"), fill = c("blue","red"))
-#the York regression line and the OLS regression line both go to the "center of gravity"
-abline(v= first$mean.x,h= first$mean.y ,lty = "dashed", col = "red")
-points(x= first$mean.x,y= first$mean.y,col="red", pch = 16)
-abline(v= mean(x),h= mean(y) ,lty = "dashed", col = "blue")
-points(x= mean(x),y= mean(y),col="blue", pch = 16)
+first$plots[2]
 
-plot(first$x.residuals,first$y.residuals)
+# all.plots <- function(y, x){
+#   plot(x,y, pch = 16)
+#   lines(x,first$fitted_y , col = "red",lwd = 2)
+#   lines(x, lm_OLS$fitted.values, col = "blue", lty = "dashed",lwd = 2)
+#   legend("topright",legend = c("OLS","York"), fill = c("blue","red"))
+#   #the York regression line and the OLS regression line both go to the "center of gravity"
+#   abline(v= first$mean.x,h= first$mean.y ,lty = "dashed", col = "red")
+#   points(x= first$mean.x,y= first$mean.y,col="red", pch = 16)
+#   abline(v= mean(x),h= mean(y) ,lty = "dashed", col = "blue")
+#   points(x= mean(x),y= mean(y),col="blue", pch = 16)
+#   compare_OLS_York <- recordPlot()
+#   dev.off()
+#
+#   plot(first$x.residuals,first$y.residuals)
+#   residuals <- recordPlot()
+#   dev.off()
+#   return(list("compare_OLS_York" = compare_OLS_York, "residuals" = residuals))
+# }
+#
+# second <- all.plots(y,x)
+# second$compare_OLS_York
 
 ## end of (relevant) script
 
