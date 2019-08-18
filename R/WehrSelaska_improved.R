2c32df8e2865f5ede6a09c675ce92353e0cfc890
Jonas Cedro Delgado <j.cedro_delgado@web.de>
2019-08-18 12:26:26 +0200

    wehr and selaska nochmal verbessert

--- R/WehrSelaska_improved.R
+++ R/WehrSelaska_improved.R
@@ -1,7 +1,7 @@
 ### york in Least Squares Fitting Of A Straight Line With Correlated Errors ###
 
 ## Input from Table I and Table II in york 1966
-
+rm(list=ls())
 x <- c(0,0.9, 1.8, 2.6, 3.3, 4.4, 5.2, 6.1, 6.5, 7.4)
 y <- c(5.9, 5.4, 4.4, 4.6, 3.5, 3.7, 2.8, 2.8, 2.4, 1.5)
 
@@ -11,9 +11,11 @@ my_w <- data.frame(
 
 ## function for algo
 
+#initial value of b is OLS
+lm_OLS <- lm(y~x)
+slope <- as.numeric(lm_OLS[[1]][2])
+
 york <- function(y, x, tolerance = 1e-10, weights){
-  #initial value of b is OLS
-  slope <- as.numeric(lm(y~x)[[1]][2])
 
   slope_diff <- 10
   count <- 0
@@ -66,16 +68,27 @@ york <- function(y, x, tolerance = 1e-10, weights){
   chisq_weight <- chisq_weight / (length(x) - 2)
   sigma_x <- sqrt(2 / (length(x) - 2))
 
-  residuals <- y - (intercept + slope * x)
+  fitted_y <- intercept + slope * x
+
+  residuals <- y - fitted_y
 
   mt <- matrix(c(intercept, slope, sigma_intercept, sigma_slope), nrow = 2)
   rownames(mt) <- c("intercept", "slope")
   colnames(mt) <- c("Estimate", "Std.Error")
-  est <- list("coefficients" = mt, "weighting.vector" = Weight, "residuals" = residuals, "mean.x" = x_bar, "mean.y" = y_bar ,"Std.Error.chi" = sigma_x, "iterations" = count, x_centered, y_centered, x, y, x_mean, "show" = x_adj)
+  est <- list("coefficients" = mt, "weighting.vector" = Weight, "residuals" = residuals,"fitted_y"=fitted_y, "mean.x" = x_bar, "mean.y" = y_bar ,"Std.Error.chi" = sigma_x, "iterations" = count, x_centered, y_centered, x, y, x_mean, "show" = x_adj)
   return(est)
 }
 
 first <- york(y, x, weight = my_w)
+first
+
+plot(x,y)
+lines(x,first$fitted_y , col = "red",lwd = 2)
+lines(x, lm_OLS$fitted.values, col = "blue", lty = "dashed",lwd = 2)
+legend("topright",legend = c("OLS","York"), fill = c("blue","red"))
+#the York regression line and the OLS regression line both go to the "center of gravity"
+abline(v= first$mean.x,h= first$mean.y ,lty = "dashed", col = "red")
+abline(v= mean(x),h= mean(y) ,lty = "dashed", col = "blue")
 
 ## end of (relevant) script
 
