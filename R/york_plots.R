afd678b7fcfb1b21f1b587408cf6f09c8f1c56e1
Jonas Cedro Delgado <j.cedro_delgado@web.de>
2019-08-19 17:33:41 +0200

    erweitert

--- R/york_plots.R
+++ R/york_plots.R
@@ -8,15 +8,15 @@ york.plots <- function(york.output){
        las = 1,
        ylim = c(1.5,6))
 
-  lines(york.output$original.x.values, york.output$fitted_y , col = "red",lwd = 2)
-  lines(york.output$original.x.values, york.output$fitted_ols, col = "blue", lty = "dashed",lwd = 2)
+  lines(york.output$original.x.values, york.output$fitted.y , col = "red",lwd = 2)
+  lines(york.output$original.x.values, york.output$fitted.ols, col = "blue", lty = "dashed",lwd = 2)
   legend("topright",legend = c("OLS","York"), fill = c("blue","red"))
   #the York regression line and the OLS regression line both go to the "center of gravity"
   abline(v = york.output$mean.x, h = york.output$mean.y, lty = "dashed", col = "red")
   points(x = york.output$mean.x, y = york.output$mean.y, col = "red", pch = 16)
   abline(v = mean(york.output$original.x.values), h = mean(york.output$original.y.values) ,lty = "dashed", col = "blue")
   points(x = mean(york.output$original.x.values), y = mean(york.output$original.y.values), col = "blue", pch = 16)
-  compare_OLS_York <- recordPlot()
+  compare.OLS.York <- recordPlot()
   dev.off()
 
   plot(york.output$x.residuals, york.output$y.residuals,
@@ -29,7 +29,7 @@ york.plots <- function(york.output){
   residuals <- recordPlot()
   dev.off()
 
-  plot(york.output$fitted_y, york.output$x.residuals,
+  plot(york.output$fitted.y, york.output$x.residuals,
        xlab = "fitted y",
        ylab = "x residuals",
        main = "x residuals vs. fitted plot",
@@ -37,10 +37,10 @@ york.plots <- function(york.output){
        pch = 16,
        las = 1)
 
-  xresiduals_vs_fitted <- recordPlot()
+  xresiduals.vs.fitted <- recordPlot()
   dev.off()
 
-  plot(york.output$fitted_y, york.output$y.residuals,
+  plot(york.output$fitted.y, york.output$y.residuals,
        xlab = "fitted y",
        ylab = "y residuals",
        main = "y residuals vs. fitted plot",
@@ -48,13 +48,13 @@ york.plots <- function(york.output){
        pch = 16,
        las = 1)
 
-  yresiduals_vs_fitted <- recordPlot()
+  yresiduals.vs.fitted <- recordPlot()
   dev.off()
 
-  return(list("compare_OLS_York_with_center_of_gravity" = compare_OLS_York, "residuals" = residuals, "yresiduals_vs_fitted" = yresiduals_vs_fitted.))
+  return(list("compare.OLS.York.with.center.of.gravity" = compare.OLS.York, "residuals" = residuals, "yresiduals.vs.fitted" = yresiduals.vs.fitted.))
 }
 
-my_plots <- york.plots(york.output)
+my.plots <- york.plots(york.output)
 
-my_plots$compare_OLS_York_with_center_of_gravity
-my_plots$residuals
+my.plots$compare.OLS.York.with.center.of.gravity
+my.plots$residuals
