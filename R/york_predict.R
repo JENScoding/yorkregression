0aef59e7f1899509742a910f0da04919fecd7269
Jonas Cedro Delgado <j.cedro_delgado@web.de>
2019-09-12 01:42:20 +0200

    fix

--- R/york_predict.R
+++ R/york_predict.R
@@ -1,7 +1,7 @@
 ### york in Least Squares Fitting Of A Straight Line With Correlated Errors ###
 ## Prediction function
 
-#' @title Predict Method for York Fits
+#' @title Predict method for York fits
 #'
 #' @description Obtains predictions from a fitted York's regression.
 #'
@@ -23,10 +23,9 @@
 #' y <- c(5.9, 5.4, 4.4, 4.6, 3.5, 3.7, 2.8, 2.8, 2.4, 1.5)
 #' weights.x <- c(1e+3, 1e+3, 5e+2, 8e+2, 2e+2, 8e+1, 6e+1, 2e+1, 1.8, 1)
 #' weights.y <- c(1, 1.8, 4, 8, 20, 20, 70, 70, 1e+2, 5e+2)
-#' fit <- york(x = x, y = y, weights.x = weights.x, weights.y = weights.y)
-#'
-#' # predict with:
-#' data<- c(2,3,4,3.6)
+#' fit <- york(x = x, y = y, weights.x = weights.x, weights.y = weights.y,
+#' r.xy = 0)
+#' data <- c(2,3,4,3.6)
 #' york.predict(york.output = fit, newdata = data)
 #'
 #' @name york.predict
