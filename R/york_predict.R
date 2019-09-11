ed2ca2cc6cde0d5d4d0892aa3418430f1a838472
JENScoding <jens.lichter@posteo.de>
2019-09-11 17:53:08 +0200

    Updating documentation, also style; Renew prediction function and have test for it; Remove Examples

--- R/york_predict.R
+++ R/york_predict.R
@@ -3,14 +3,15 @@ york.predict <- function(york.output, new) {
   if (class(york.output) != "york") {
     stop("Input must be of class york (Output of york function)")
   }
-  york.output$coefficients.york[1, 1] + york.output$coefficients.york[2, 1] * new
+  predict.y <- data.frame("x" = new, "predicted.y" = york.output$coefficients.york[1, 1] + york.output$coefficients.york[2, 1] * new)
+  colnames(predict.y) <- c("x", "predicted.y")
+
+  output <- list("prediction" = predict.y)
+  attr(output, "class") <- "york"
+  return(output)
 }
 
-# new <- c(2,3,4,3.6)
-# york.predict(york.output = york.output, new = new)
-#
-#
-# york.predict(york.output = york.output, new = 1.5)
+
 
 
 
