2e0bdcf49d5aeb99adbb49f7d289673b762ffd81
JENScoding <jens.lichter@posteo.de>
2019-09-15 19:01:02 +0200

    restructure york 66 test

---
+++ tests/testthat/test_york_66_solution.R
@@ -0,0 +1,26 @@
+test_that("Test implementation of exact solution", {
+  ## Input from Table I and Table II in York 1966
+  x <- c(0,0.9, 1.8, 2.6, 3.3, 4.4, 5.2, 6.1, 6.5, 7.4)
+  y <- c(5.9, 5.4, 4.4, 4.6, 3.5, 3.7, 2.8, 2.8, 2.4, 1.5)
+
+  weights.y = c(1, 1.8, 4, 8, 20, 20, 70, 70, 1e+2, 5e+2)
+  weights.x = c(1e+3, 1e+3, 5e+2, 8e+2, 2e+2, 8e+1, 6e+1, 2e+1, 1.8, 1)
+
+  ## Test
+  expect_error(york(x, y, weights.x = weights.x, weights.y = weights.y, r.xy = 0, mult.samples = F, approx.solution = T), NA)
+
+  first <- york(x, y, weights.x = 1, weights.y = 1, r.xy = 0)
+  expect_equal(round(first$coefficients[2,1], 3),
+               -0.546)
+  second <- york(x, y, weights.x = 1e10, weights.y = 1, r.xy = 0)
+  expect_equal(round(second$coefficients[2,1], 3),
+               round(second$ols.summary$coefficients.ols[2,1], 3))
+  third <- york(x, y, weights.x = weights.x, weights.y = weights.y, r.xy = 0, mult.samples = F, approx.solution = T)
+  expect_type(third$coefficients[2, 1], "double")
+  expect_true(third$coefficients[2, 1] < -0.477 && third$coefficients[2, 1] > -0.478)
+})
+
+
+
+
+
