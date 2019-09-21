7f049228ee4a169bf6eaf9900121eb5d8184a8a7
JENScoding <jens.lichter@posteo.de>
2019-09-21 23:02:11 +0200

    Outsourcing parts in method_plot to method_plot_internal; finalise the influential outlier plot and the chi-squared plot

--- R/method_plot.R
+++ R/method_plot.R
@@ -42,10 +42,11 @@
 #' @importFrom
 #'  ggplot2 ggplot aes geom_abline geom_point labs theme element_text
 #'  draw_key_rect scale_colour_manual geom_vline geom_hline geom_smooth geom_line
+#'  stat_function geom_segment annotate
 #' @importFrom
 #'  utils stack
 #' @importFrom
-#'  stats pnorm
+#'  stats pnorm qchisq dchisq
 #'
 #' @method
 #'  plot york
@@ -70,25 +71,30 @@ plot.york <- function(x, ...) {
     y_data <- york_fit$data[, 2]
 
     ddf <- data.frame(x = x_data, y = y_data)
+
+    # plot fitted line and data
     plot_1 <- ggplot(data = ddf, aes(x = x,
                                    y = y)) +
+      geom_point() +
       geom_abline(aes(slope = york_fit$coefficients[2, 1],
                       intercept = york_fit$coefficients[1, 1]), col = "red") +
-      geom_point() +
       labs(title="York's best-fit straight line",
            x ="x data", y = "y data") +
       theme(plot.title =element_text(hjust = 0.5))
-  } else {
+
+  } else { # mult.samples = TRUE
 
     x_data <- stack(york_fit$data$x)[, 1]
     y_data <- stack(york_fit$data$y)[, 1]
 
     ddf <- data.frame(x = x_data, y = y_data)
+
+    # plot fitted line and data in multiple sample case
     plot_1 <- ggplot(data = ddf, aes(x = x,
                                      y = y)) +
+      geom_point() +
       geom_abline(aes(slope = york_fit$coefficients[2, 1],
                       intercept = york_fit$coefficients[1, 1]), col = "red") +
-      geom_point() +
       labs(title="York's best-fit straight line",
            x ="x data", y = "y data") +
       theme(plot.title = element_text(hjust = 0.5))
@@ -109,6 +115,7 @@ plot.york <- function(x, ...) {
   # plot york, orthogonal and OLS line
   plot_2 <- ggplot(data = ddf2, aes(x = x,
                                     y = y)) +
+    geom_point() +
     geom_abline(aes(slope = york_fit$coefficients[2, 1],
                     intercept = york_fit$coefficients[1, 1], colour = "York"),
                 key_glyph = draw_key_rect) +
@@ -117,11 +124,6 @@ plot.york <- function(x, ...) {
                     colour = "OLS")) +
     geom_abline(aes(slope = orthogonal$coefficients[2, 1], intercept =
                       orthogonal$coefficients[1, 1], colour = "Orthogonal")) +
-    labs(colour = "") +
-    scale_colour_manual(values=c("blue", "green", "red")) +
-    geom_point() +
-    labs(title="York' best fit straight line compared to OLS and orthogonal ",
-         x ="x data", y = "y data") +
     geom_vline(xintercept = york_fit$weighted_mean_x, linetype = "dashed",
                color = "red", size = 0.4) +
     geom_hline(yintercept = york_fit$weighted_mean_y, linetype = "dashed",
@@ -134,6 +136,9 @@ plot.york <- function(x, ...) {
                    y = mean_y), col = "blue") +
     geom_point(aes(x = york_fit$weighted_mean_x,
                    y = york_fit$weighted_mean_y), col = "red") +
+    scale_colour_manual(values=c("blue", "green", "red")) +
+    labs(title="York' best fit straight line compared to OLS and orthogonal ",
+         x ="x data", y = "y data", colour = "") +
     theme(plot.title = element_text(hjust = 0.5))
 
 
@@ -144,10 +149,10 @@ plot.york <- function(x, ...) {
   # plot x and y residuals
   plot_3 <- ggplot(aes(x = x, y = y), data = ddf3) +
     geom_point() +
-    labs( title = "y residuals vs. x residuals",
-          x = "x residuals", y = "y residuals") +
     geom_hline(yintercept = 0, linetype = "dashed", col = "red") +
     geom_vline(xintercept = 0, linetype = "dashed", col = "red") +
+    labs( title = "y residuals vs. x residuals",
+          x = "x residuals", y = "y residuals") +
     theme(plot.title = element_text(hjust = 0.5))
 
 
@@ -158,9 +163,9 @@ plot.york <- function(x, ...) {
   # plot x resid vs fitted y
   plot_4 <- ggplot(aes(x = x, y = y), data = ddf4) +
     geom_point() +
-    labs( title = "x residuals vs. fitted y",
-          x = "fitted y", y = "x residuals") +
     geom_hline(yintercept = 0, linetype = "dashed", col = "red") +
+    labs(title = "x residuals vs. fitted y",
+          x = "fitted y", y = "x residuals") +
     theme(plot.title = element_text(hjust = 0.5))
 
 
@@ -171,91 +176,132 @@ plot.york <- function(x, ...) {
   # plot y resid vs fitted y
   plot_5 <- ggplot(aes(x = x, y = y), data = ddf5) +
     geom_point() +
+    geom_hline(yintercept = 0, linetype = "dashed", col = "red") +
     labs(title = "y residuals vs. fitted y",
          x = "fitted y", y = "y residuals") +
-    geom_hline(yintercept = 0, linetype = "dashed", col = "red") +
     theme(plot.title = element_text(hjust = 0.5))
 
 
-  ### Chi^2 test graph and test results
-  # set up parameters for visualisation
-  df <- york_fit$goodness_of_fit$chisq_df
-  options(scipen = 999)
-  p_value <- round(york_fit$goodness_of_fit$p_value, 5)
-  math <- " = "
-  if (p_value < 0.00001) {
-    p_value <- 0.00001
-    math <- " < "
+  ### detect influential observations
+  ###  -> assume asymptotic normality of slope coefficients)
+  if (york_fit$york_arguments$mult_samples == FALSE) {
+    sd_x <- york_fit$data[, 3]
+    sd_y <- york_fit$data[, 4]
+    r_xy_errors <- york_fit$data[, 5]
+    x_mult <- NULL
+    y_mult <- NULL
+  } else { # mult_sample = TRUE
+    sd_x <- york_fit$data$sd_x
+    sd_y <- york_fit$data$sd_y
+    r_xy_errors <- york_fit$data$error_correlation
+    x_mult <- york_fit$data$x
+    y_mult <- york_fit$data$y
+  }
+  influential <- f_p_influential(x_data, y_data,
+                                 york_fit$york_arguments$mult_samples,
+                                 sd_x, sd_y,
+                                 r_xy_errors,
+                                 york_fit$coefficients[2, 1],
+                                 york_fit$coefficients[2, 2],
+                                 york_fit$york_arguments$tolerance,
+                                 york_fit$york_arguments$max_iterations,
+                                 x_mult, y_mult)
+
+  if (length(influential) == 0) {
+    plot_6 <- NULL
+  } else {
+    ddf6 <- data.frame(x = x_data, y = y_data)
+    ddf_influential <- data.frame(influential$detect_influential)
+
+    # plot fitted line and observations with encircled influential points
+    plot_6 <- ggplot(data = ddf6, aes(x = x,
+                                      y = y)) +
+      geom_point() +
+      geom_abline(aes(slope = york_fit$coefficients[2, 1],
+                      intercept = york_fit$coefficients[1, 1]), col = "red") +
+      geom_point(aes(x = ddf_influential$x,
+                     y = ddf_influential$y,
+                     col = ddf_influential$which_row),
+                 size = 15, shape = 1,
+                 data = ddf_influential) +
+      labs(title=paste("York's best-fit straight line with encircled ",
+                       "potential influential points"),
+           subtitle = "significance level alpha = 0.01",
+           x ="x data", y = "y data", col = influential$leg_title) +
+      theme(plot.title =element_text(hjust = 0.5)) +
+      theme(plot.subtitle =element_text(hjust = 0.5))
   }
 
-  # calculate corresponding x and y values of the p-values,
-  # the critical value and rejection region for the test (alpha = 0.01)
-  x_p_value <- qchisq(1 - p_value, df)
-  critical_value <- qchisq(c(0.9, 0.95, 0.99,
-                             seq(0.9915, 0.995, length.out = 15),
-                             seq(0.99501, 0.999, length.out = 14)), df)
-  y_critical <- dchisq(critical_value, df)
-  y_p <- dchisq(x_p_value, df)
-  reject_region <- data.frame("x" = critical_value[4:32],
-                              "y" = rep(0, length(critical_value) - 3),
-                              "yend" = y_critical[4:32])
-  t_statistic <- round(york_fit$goodness_of_fit$chisq_statistic, 3)
-  if (t_statistic >= 100){
-    adj <- 1.2
-  } else {
-    adj <- 1.28
-  }
+
+  ### Chi^2 test graph and test results
+  chisq_test <- f_p_chisq_test(york_fit$goodness_of_fit$chisq_df,
+                               york_fit$goodness_of_fit$p_value,
+                               york_fit$goodness_of_fit$chisq_statistic)
+  ddf_reject_region <- data.frame(chisq_test$reject_region)
 
   # plot chisq graph and test results
-  plot_6 <- ggplot(data = data.frame(x = c(0, 3 * df)), aes(x)) +
-    stat_function(fun = dchisq, n = 1e+3, args = list(df = df), size = 1.1) +
-    geom_hline(yintercept = 0) +
-    geom_vline(xintercept = 0) +
-    geom_segment(aes(x = x_p_value, y = 0,
-                     xend = x_p_value, yend = y_p,
-                     colour = "Test Statistic")) +
-    geom_segment(aes(x = critical_value[1], y =  0,
-                     xend = critical_value[1], yend = y_critical[1],
-                     colour = "Critical Value, alpha = 0.1"),
+  plot_7 <- ggplot(data = data.frame(x = c(0, chisq_test$x_limit)), aes(x)) +
+    stat_function(fun = dchisq, n = 1e+3, args = list(df = chisq_test$df),
+                  size = 1.1) +
+    geom_hline(yintercept = 0, col = "black") +
+    geom_vline(xintercept = 0, col = "black") +
+    geom_segment(aes(x = chisq_test$x_p_value,
+                     y = 0,
+                     xend = chisq_test$x_p_value,
+                     yend = chisq_test$y_p_value,
+                     colour = "test statistic")) +
+    geom_segment(aes(x = chisq_test$critical_value[1],
+                     y =  0,
+                     xend = chisq_test$critical_value[1],
+                     yend = chisq_test$y_critical[1],
+                     colour = "critical value, alpha = 0.1"),
                  alpha = 0.8) +
-    geom_segment(aes(x = critical_value[2], y =  0,
-                     xend = critical_value[2], yend = y_critical[2],
-                     colour = "Critical Value, alpha = 0.05")) +
-    geom_segment(aes(x = critical_value[3], y =  0,
-                     xend = critical_value[3], yend = y_critical[3],
-                     colour = "Critical Value, alpha = 0.01")) +
-    geom_segment(aes(x = x, y =  y,
-                     xend = x, yend = yend),
-                 colour = "red", alpha = 0.05, size = (15 - log(df) * 2.5),
-                 data = reject_region) +
+    geom_segment(aes(x = chisq_test$critical_value[2],
+                     y =  0,
+                     xend = chisq_test$critical_value[2],
+                     yend = chisq_test$y_critical[2],
+                     colour = "critical value, alpha = 0.05")) +
+    geom_segment(aes(x = chisq_test$critical_value[3],
+                     y =  0,
+                     xend = chisq_test$critical_value[3],
+                     yend = chisq_test$y_critical[3],
+                     colour = "critical value, alpha = 0.01")) +
+    geom_segment(aes(x = ddf_reject_region$x,
+                     y = ddf_reject_region$y,
+                     xend = ddf_reject_region$x,
+                     yend = ddf_reject_region$yend),
+                 colour = "red", alpha = 0.05,
+                 size = (15 - log(chisq_test$df) * 2.5),
+                 data = ddf_reject_region) +
     scale_colour_manual("", values = c("red4", "red3", "red", "lightgreen")) +
-    annotate("text", label = paste("T-Statistic = ", t_statistic),
-             family = "Times New Roman", x =  Inf, y = Inf,
-             vjust = 7.5, hjust = adj, size = 6) +
-    annotate("text", label = paste("p-value", math, p_value ),
-             family = "Times New Roman", x =  Inf, y = Inf,
+    annotate("text", label = paste("test statistic = ", chisq_test$test_statistic),
+             x =  Inf, y = Inf,
+             vjust = 7.5, hjust = chisq_test$adj, size = 6) +
+    annotate("text", label = paste("p-value", chisq_test$math, chisq_test$p_value ),
+              x =  Inf, y = Inf,
              vjust = 10, hjust = 1.3, size = 6) +
-    labs(title = paste("Chi^2 test: ", york_fit$goodness_of_fit$test_result),
-         x = "x", y = "Probability") +
+    labs(title = paste("Chi-squared test: ", york_fit$goodness_of_fit$test_result),
+         x = "x", y = "Density") +
     theme(plot.title = element_text(hjust = 0.5))
 
 
   ### Trace plot -> Convergence of slope
   if (york_fit$york_arguments$approx_solution == FALSE) {
-    ddf7 <- data.frame(x = 1:york_fit$n_iterations,
-                       y = york_fit$slope_per_iteration[,1])
+    ddf8 <- data.frame(x = c(0, 1:york_fit$n_iterations),
+                       y = c(york_fit$ols_summary$coefficients_ols[2, 1],
+                             york_fit$slope_per_iteration[,1]))
 
     # plot trace plot
-    plot_7 <- ggplot(aes(x = x, y = y), data = ddf7)+
+    plot_8 <- ggplot(aes(x = x, y = y), data = ddf8) +
       geom_line() +
       geom_point() +
+      geom_hline(yintercept = york_fit$coefficients[2,1], col = "darkblue") +
       labs(title = "Trace plot",
            x = "Number of iterations", y = "slope coefficient") +
-      geom_hline(yintercept = york_fit$coefficients[2,1], col = "darkblue") +
       theme(plot.title = element_text(hjust = 0.5))
   } else { # approx_solution = TRUE
-    plot_7 <- NULL
+    plot_8 <- NULL
   }
-  return(list(plot_1, plot_2, plot_3, plot_4, plot_5, plot_6, plot_7))
+  return(list(plot_1, plot_2, plot_3, plot_4, plot_5, plot_6, plot_7, plot_8))
 }
 
---
+++ R/method_plot_internal.R
@@ -0,0 +1,152 @@
+#' @title
+#'  Internal Functions for Plot Method
+#'
+#' @description
+#'  Functions used in plot.york method. For functions in "Internal Functions
+#'  for Plot Method" the output is used for proceeding calculations or
+#'  displaying in the plot.york method. All the functions in "Internal
+#'  Functions for Plot Method" begin with f_p_ and then the name of the
+#'  function is given.
+#'
+#'  f_p_influential determines influential points in a york regression.
+#'  An influential point is defined as a point that significantly changes
+#'  the slope coefficient if omitted. Assymptotic normality of the slope
+#'  coefficient is assumed. The test is based on a significance level
+#'  of alpha = 0.01.
+#'
+#'  f_p_chisq_test is a function that calculates the critical values for
+#'  alpha = c(0.1, 0.05, 0.01) and the corresponding probabilities in a
+#'  \eqn{\chi^2}--test. Additionally, values for the rejection region
+#'  for alpha = 0.01 are given.
+#'
+#'
+#' @name method_plot_internal
+#'
+#' @keywords
+#'  internal
+#'
+
+# function to determine influential observations:
+f_p_influential <- function(x_data, y_data, mult_samples,
+                        sd_x, sd_y, r_xy_errors, coef, coef_se,
+                        tolerance, max_iterations, x_mult, y_mult) {
+
+  # first for non multiple case then for multiple
+  if (mult_samples == FALSE) {
+
+    # run york regression and omit one observation
+    slope_influential <- NULL
+    for (i in 1:length(x_data)) {
+      slope_influential[i] <- york(x_data[-i], y_data[-i],
+                               sd_x = sd_x[-i],
+                               sd_y = sd_y[-i],
+                               r_xy_errors = r_xy_errors[-i],
+                               tolerance = tolerance,
+                               max_iterations = max_iterations)[[1]][2, 1]
+    }
+
+    # determine significance
+    t_statistic <- (coef - slope_influential) /
+      coef_se
+    p_value <- 2 * (1 - pnorm(abs(t_statistic)))
+    detect_influential <- which(p_value <= 0.01)
+
+    # write all influential observations with x and y coordinate in a data frame
+    influential <- data.frame("x" = x_data[detect_influential],
+                          "y" = y_data[detect_influential],
+                          "which_row" = factor(detect_influential))
+    leg_title = "Influential observation at observation point:"
+  } else { # mult.samples = TRUE
+
+    # run york regression and omit one row of observations
+    slope_influential <- NULL
+    for (i in 1:nrow(x_mult)) {
+      slope_influential[i] <- suppressWarnings(
+        york(x_mult[-i,], y_mult[-i,],
+             tolerance = tolerance,
+             max_iterations =max_iterations,
+             mult_samples = mult_samples)[[1]][2, 1])
+    }
+
+    # determine significance
+    t_statistic <- (coef - slope_influential) /
+      coef_se
+    p_value <- 2 * (1 - pnorm(abs(t_statistic)))
+    detect_influential <- which(p_value <= 0.01)
+
+    # define output
+    x_mult_t <- data.frame(t(x_mult[detect_influential,]))
+    y_mult_t <- data.frame(t(y_mult[detect_influential,]))
+
+    # write all influentials with x and y coordinate in a data frame
+    influential <- data.frame("x" = stack(x_mult_t)[1],
+                              "y" = stack(y_mult_t)[1],
+                              "which_row" = factor(rep(detect_influential,
+                                                       each =
+                                                         ncol(x_mult))))
+    colnames(influential) <- c("x", "y", "which_row")
+    leg_title = "Influential observation(s) in row(s):"
+  }
+
+  output <- list("detect_influential" = influential,
+                 "leg_title" = leg_title)
+
+  return(output)
+}
+
+
+#' @title
+#'  Internal Functions for Plot Method
+#'
+#' @name method_plot_internal
+#'
+#' @keywords
+#'  internal
+#'
+# function for Chi^2 test with test results
+f_p_chisq_test <- function(chisq_df, p_value, chisq_statistic) {
+
+  # set up parameters for visualisation
+  df <- chisq_df
+  options(scipen = 999)
+  p_value <- round(p_value, 5)
+  math <- " = "
+  if (p_value < 0.00001) {
+    p_value <- 0.00001
+    math <- " < "
+  }
+
+  # calculate corresponding x and y values of the p-values,
+  # the critical value and rejection region for the test (alpha = 0.01)
+  x_p_value <- qchisq(1 - p_value, df)
+  critical_value <- qchisq(c(0.9, 0.95, 0.99,
+                             seq(0.9915, 0.995, length.out = 15),
+                             seq(0.99501, 0.999, length.out = 14)), df)
+  y_critical <- dchisq(critical_value, df)
+  y_p_value <- dchisq(x_p_value, df)
+  reject_region <- data.frame("x" = critical_value[4:32],
+                              "y" = rep(0, length(critical_value) - 3),
+                              "yend" = y_critical[4:32])
+  test_statistic <- round(chisq_statistic, 3)
+  if (test_statistic >= 100){
+    adj <- 1.09
+  } else {
+    adj <- 1.17
+  }
+
+  x_limit <- (2 * (1 + log(df^2) / df)) * df
+
+  output <- list("df" = df,
+                 "test_statistic" = test_statistic,
+                 "p_value" = p_value,
+                 "x_p_value" = x_p_value,
+                 "y_p_value" = y_p_value,
+                 "critical_value" = critical_value,
+                 "y_critical" = y_critical,
+                 "reject_region" = reject_region,
+                 "adj" = adj,
+                 "math" = math,
+                 "x_limit" = x_limit)
+
+  return(output)
+}
