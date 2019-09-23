01163702b98c9d832ea4d98b5559071694acb3d5
JENScoding <jens.lichter@posteo.de>
2019-09-23 02:29:34 +0200

    Changed package name

---
+++ R/package_documentation.R
@@ -0,0 +1,25 @@
+#' yorkregression: A package to fit a linear York's model.
+#'
+#' The yorkregression package provides one main function named york and three methods
+#' summary.york, plot.york and predict.york.
+#'
+#' @section
+#'  york function:
+#'  The york function fits a York model to the data. It is used when
+#'  both X and Y variables are subject to measurement error. Returned
+#'  is an object of class york.
+#'
+#' @section
+#'  york methods:
+#'  The summary.york method returns the summary statistics of a fitted york
+#'  model.
+#'
+#'  The plot.york method returns plots for the fitted york model.
+#'
+#'  The predict.york method returns predicted values for y for a fitted york
+#'  model.
+#'
+#' @docType package
+#'
+#' @name yorkregression
+NULL
